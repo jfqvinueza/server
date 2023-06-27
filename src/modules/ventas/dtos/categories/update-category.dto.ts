da0284c1cd1cd91d539c8d785ef89cc411c02ea0
JimmyVin <jimmy-dragon1@hotmail.com>
2023-06-27 08:03:37 -0500

    first commit

--- src/modules/ventas/dtos/categories/update-category.dto.ts
+++ src/modules/ventas/dtos/categories/update-category.dto.ts
@@ -1,5 +1,5 @@
-import { PartialType } from "@nestjs/swagger";
-import { BaseCategoryDto } from "./base-category.dto";
+import { PartialType } from '@nestjs/swagger';
+import { BaseCategoryDto } from './base-category.dto';
 
 export class UpdateCategoryDto extends PartialType(BaseCategoryDto){
 
